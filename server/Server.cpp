c67543e89d53236c43d3d3c16b801fda4eb2f1cd
Mohammed Ymik <mymik@e2r8p1.1337.ma>
2021-06-10 15:51:00 +0100

    ok

--- server/Server.cpp
+++ server/Server.cpp
@@ -1,4 +1,9 @@
 #include "Server.hpp"
+#include "poll.h"
+#include <iostream>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
 Server& Server::getInstance() {
     if (!_instance) {
@@ -75,93 +80,55 @@ std::vector<ServerSocket>::iterator Server::isServerSocket(int fd) {
 }
 
 void Server::acceptConnections() {
-    std::vector<ServerSocket>::iterator it = _sockets.begin();
-    // FD_ZERO(&master_set);
-    // FD_ZERO(&response_set);
-    // addServerSocketsToSet();
+    bool running = true;
+    std::vector<int> _sockets_fd;
+    int serverSocketCount = 0;
 
-        // worker_set = master_set;
-        // response_set = master_set;
-        // std::cout << "Select Waiting" << std::endl;
-        // int select_ret = select(FD_SETSIZE, &worker_set, &response_set, NULL, NULL);
-        // if (select_ret == 0)
-        // {
-        //     std::cout << "select 0" << std::endl;
-        //     continue;
-        // }
-        // // std::cout << "ret = " << select_ret << std::endl;
-        // for (int i = 0; i < FD_SETSIZE; i++) {
-        //     if (FD_ISSET(i, &response_set)) {
-        //         std::string _str = "HTTP/1.1 200 OK\r\nContent-Type:text/html\r\nContent-Length: 10\r\nHello world";
-        //         send(i, _str.c_str(), _str.size(), 0);
-        //     }
-        //     // if ready to read or accept a socket
-        //     if (FD_ISSET(i, &worker_set)) {
-        //         if ((it =isServerSocket(i)) != _sockets.end()) {
-        //             int conn = it->acceptConnection();
-        //             FD_SET(conn, &master_set);
-        //         } else {
-        //             RequestReader reader;
-        //             reader.readConnection(i);
-        //         }
-        //     }
+    // add server sockets
+    std::vector<ServerSocket>::iterator it;
+    for (it = _sockets.begin(); it != _sockets.end(); it++) {
+        _sockets_fd.push_back(it->getFd());
+        serverSocketCount++;
+    }
 
-        // }
-        // fd_set read_set;
-        // fd_set write_set;
-
-        // FD_ZERO(&read_set);
-        // FD_ZERO(&write_set);
-
-        // std::vector<ServerSocket>::iterator it;
-        // for (it = _sockets.begin(); it != _sockets.end(); it++) {
-        //     if (it->getFd() != -1) {
-        //         FD_SET(it->getFd(), &read_set);
-        //     }
-        // }
-
-        while (true)
-        {
-            // struct timeval t = { 5, 0 };
-            // std::cout << "Select" << std::endl;
-            // int select_ret = select(FD_SETSIZE, &read_set, &write_set, NULL, NULL);
-            // if (select_ret == 0)
-            //     continue;
-            // for (size_t i = 0; i < FD_SETSIZE; i++)
-            // {
-            //     if (FD_ISSET(i, &read_set)) {
-            //         if (FD_ISSET(i, &worker_set)) {
-            //             if ((it =isServerSocket(i)) != _sockets.end()) {
-            //                 int conn = it->acceptConnection();
-            //                 std::cout << "Got a connection" << std::endl;
-            //                 FD_SET(conn, &read_set);
-            //             } else {
-            //                 std::cout << "Conn i should read from" << std::endl;
-            //             }
-            //         }
-            //     }
-            // }
-
-            std::cout << it->getPort() << std::endl;
-            int conn = it->acceptConnection();
-            char buffer[1024];
-            int ret;
-            std::string reqContent;
-
-            while ((ret = read(conn, buffer, sizeof(buffer) - 1)) == 1023) {
-                std::cout << ret << std::endl;
-                buffer[ret] = '\0';
-                std::string tmp = "";
-                tmp.assign(buffer);
-                reqContent += tmp;
-            }
-            
-            std::string tmp = "";
-            tmp.assign(buffer);
-            reqContent += tmp;
-
-            std::cout << reqContent << std::endl;
-            std::cout << "End" << std::endl;
-            close(conn);
+    while (running)
+    {
+        struct pollfd fds[_sockets_fd.size()];
+        for (int i = 0; i < _sockets_fd.size(); i++) {
+            fds[i].fd = _sockets_fd[i];
+            fds[i].events = POLLIN;
         }
+        log "Poll" line;
+        int n = poll(fds, _sockets_fd.size(), 2000);
+        if (n == 0)
+            continue;
+        for (int i = 0; i < _sockets_fd.size(); i++) {
+            if (fds[i].revents & POLLIN) {
+                if (i < serverSocketCount) {
+                    int conn = _sockets[i].acceptConnection();
+                    _sockets_fd.push_back(conn);
+                    fcntl(conn, F_SETFL, O_NONBLOCK);
+                    std::cout << "Got a new connection" << std::endl;
+                } else {
+                    std::cout << "Connection i should read from" << std::endl;
+                    int ret;
+                    char buffer[1024];
+                    std::string reqContent = "";
+                    while ((ret = recv(fds[i].fd, buffer, 1023, 0)) == 1024) {
+                        std::string tmp;
+                        buffer[ret] = '\0';
+                        tmp.assign(buffer);
+                        reqContent += tmp;
+                    }
+                    if (ret != -1) {
+                        std::string tmp;
+                        buffer[ret] = '\0';
+                        tmp.assign(buffer);
+                        reqContent += tmp;
+                        std::cout << reqContent << std::endl;
+                    }
+                }
+            }
+        }
+    }
 }
